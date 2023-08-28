3c8e57bbd7591535ee80eb2374568f0697fa003f
Anna Bystrova <aabystrovy@mail.ru>
2023-08-28 22:32:51 +0300

    Files aded

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int price = 13676; // цена билета
+
+        int rub = 20; // рублей за 1 милю
+
+        System.out.println("При данной стоимости билета у Вас начислится:");
+        System.out.println((price / rub) + " бонуса (ов)");
+    }
+}
